bfa8548dbcfc3c0b2f1215988641878bb39efb9b
Napopegu <142897234+Napopegu@users.noreply.github.com>
2024-02-02 18:58:37 +0600

    выполнил задание 2

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import  add_view, subtract_view, multiply_view, divide_view, get_token_view
+from webapp.views import  add_view, subtract_view, multiply_view, divide_view, get_token_view,index
 
 
 urlpatterns = [
+    path('', index, name='index'),
     path('add/', add_view, name='add'),
     path('subtract/', subtract_view, name='subtract'),
     path('multiply/', multiply_view, name='multiply'),
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -3,12 +3,21 @@ import json
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import HttpResponse, HttpResponseNotAllowed,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
+
 
 @ensure_csrf_cookie
 def get_token_view(request, *args, **kwargs):
     if request.method =='GET':
         return HttpResponse()
     return HttpResponseNotAllowed(['GET'])
+
+def index(request):
+    return render(request, 'index.html')
+
+class Index(TemplateView):
+    template_name = 'index.html'
 
 
 def perform_operation(request, operation):
