c25e1bfb7fe0441d1ca7bdf2db6e3939c65435d6
Napopegu <142897234+Napopegu@users.noreply.github.com>
2024-02-02 18:27:12 +0600

    выполнил задание 1

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,1 +1,11 @@
-.
+from django.urls import path
+from webapp.views import  add_view, subtract_view, multiply_view, divide_view, get_token_view
+
+
+urlpatterns = [
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide'),
+    path('token/', get_token_view, name='get_token')
+]
